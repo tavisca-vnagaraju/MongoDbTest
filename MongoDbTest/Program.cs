b10a648431266ff39af4602cb45650963982995c
tavisca-vnagaraju <vnagaraju@tavisca.in>
2019-10-31 22:44:13 +0530

    Add project files.

---
+++ MongoDbTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoDbTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
